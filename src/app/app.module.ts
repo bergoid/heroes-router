b0153186cc6fd76d34df71a218f4e4a7ddfa0da2
bergoid <bergoid@users.noreply.github.com>
2021-04-14 22:01:05 +0200

    Milestone 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,31 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+// import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesModule } from './heroes/heroes.module';
-
-import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CrisisCenterModule } from './crisis-center/crisis-center.module';
+import { ComposeMessageComponent } from './compose-message/compose-message.component';
 
 @NgModule({
     imports: [
-        BrowserModule,
+        CommonModule,
+//         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
         HeroesModule,
+        CrisisCenterModule,
         AppRoutingModule
     ],
     declarations: [
         AppComponent,
-        CrisisListComponent,
-        PageNotFoundComponent
+        PageNotFoundComponent,
+        ComposeMessageComponent
     ],
     bootstrap: [ AppComponent ]
 })
